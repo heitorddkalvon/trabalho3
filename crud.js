82bbc4f5fa411bb892c851686cf16c1267c0665f
heitorddkalvon <heitorkalvon@gmail.com>
2023-05-16 20:41:55 -0300

    Alpha 1.1

--- crud.js
+++ crud.js
@@ -1,80 +1,81 @@
 let id = 0
-let tarefas = []
-
-
+let produtos = []
 
 document.querySelector("#salvar").addEventListener("click", cadastrar)
 
+window.addEventListener("load", () => {
+    produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+    produtos.forEach((produto) => {
+        document.querySelector("#produtos").innerHTML += gerarCard(produto);
+    });
+});
+
 function cadastrar() {
-  let titulo = document.querySelector("#titulo").value
-  let descricao = document.querySelector("#descricao").value
-  let pontos = document.querySelector("#pontos").value
-  let categoria = document.querySelector("#categoria").value
+    let nome = document.querySelector("#nome").value
+    let descricao = document.querySelector("#descricao").value
+    let preco = document.querySelector("#preco").value
+    let categoria = document.querySelector("#categoria").value
 
-  if (!titulo) {
-    alert("É necessário informar um título para cadastrar a tarefa.")
-    return
-  }
+    if (!nome) {
+        alert("É necessário informar um nome para cadastrar o produto.")
+        return
+    }
 
-  const tarefa = {
-    titulo,
-    descricao,
-    pontos,
-    categoria,
-  }
-  if (tarefa.titulo.length === 0) {
-    document.querySelector("#titulo").classList.add("is-invalid");
-    return;
-}
-lista_filmes.push(tarefa);
+    const produto = {
+        id: id++,
+        nome,
+        descricao,
+        preco,
+        categoria,
+    }
 
-  let tarefas = localStorage.getItem("tarefas")
-  if (tarefas) {
-    tarefas = JSON.parse(tarefas)
-  } else {
-    tarefas = []
-  }
+    produtos.push(produto)
 
-  tarefas.push(tarefa)
+    let produtosLocalStorage = localStorage.getItem("produtos")
+    if (produtosLocalStorage) {
+        produtosLocalStorage = JSON.parse(produtosLocalStorage)
+    } else {
+        produtosLocalStorage = []
+    }
 
-  localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    produtosLocalStorage.push(produto)
 
-  document.querySelector("#tarefas").innerHTML += gerarCard(tarefa)
+    localStorage.setItem("produtos", JSON.stringify(produtos))
+
+    document.querySelector("#produtos").innerHTML += gerarCard(produto)
 }
 
 function apagar(botao, id) {
-    botao.parentNode.parentNode.remove();
-    deleteCard(id);
+    botao.parentNode.parentNode.parentNode.remove()
+    deleteProduto(id)
 }
 
-function gerarCard(tarefa) {
-  return `<div class="col-12 col-md-6 col-lg-3">
+function gerarCard(produto) {
+    return `<div class="col-12 col-md-6 col-lg-3">
     <div class="card">
       <div class="card-header">
-        ${tarefa.titulo}
+        ${produto.nome}
       </div>
       <div class="card-body">
-        <p class="card-text">${tarefa.descricao}</p>
+        <p class="card-text">${produto.descricao}</p>
         <p>
           <span class="badge text-bg-warning">
-            ${tarefa.categoria}
+            ${produto.categoria}
           </span>
         </p>
-        <p>${tarefa.pontos}pts</p>
+        <p>R$ ${produto.preco}</p>
         <a href="#" class="btn btn-success">
-          <i class="bi bi-check-lg"></i>
+          <i class="bi bi-cart-check"></i>
         </a>
-        <a href="#" class="btn btn-danger" onClick="apagar(this, ${tarefa.id})">
+        <a href="#" class="btn btn-danger" onClick="apagar(this, ${produto.id})">
             <i class="bi bi-trash"></i>
         </a>
       </div>
     </div> <!-- card -->
   </div> <!-- col -->`
-
 }
 
-
-function deleteCard(id) {
-    tarefas = tarefas.filter(tarefa => tarefa.id !== id);
-    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+function deleteProduto(id) {
+    produtos = produtos.filter(produto => produto.id !== id)
+    localStorage.setItem("produtos", JSON.stringify(produtos))
 }
